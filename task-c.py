36c8a620b39b0dd3f51f28c52e2842aad8acfa0c
Time:D <darya.sam54@gmail.com>
2024-03-14 23:38:02 +0700

    Filling an array with checks

--- task-c.py
+++ task-c.py
@@ -1,2 +1,10 @@
-array_of_length_values = map(float, input("Enter side values ").split(' '))
-print(array_of_length_values)
+try:
+    array_of_length_values = map(float, input("Enter side values ").split())
+    choice_option = int(input("1. Периметр \n2. Площадь \n3. Длина средней линии \n"))
+    match choice_option:
+        case 1:
+            print(sum(array_of_length_values))
+        case 2:
+            print(*array_of_length_values)
+except ValueError:
+    print("Incorrect data")
