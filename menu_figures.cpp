43bf40e78e9af75ce52cb6cedaf905ddc8bf5d12
Time:D <darya.sam54@gmail.com>
2024-03-22 12:53:47 +0700

    Proga with rec

---
+++ menu_figures.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+
+int main() {
+    int figure_option;
+    cout << "Select a shape: \n1. Rectangle\n2. Triangle\n3. Trapezoid" << endl;
+    cin >> figure_option;
+    switch(figure_option) {
+        case 1:
+            cout << "wait";
+        case 2:
+            cout << "wait";
+        case 3:
+
+    };
+    return 0;
+}
---
+++ rectangle.cpp
@@ -0,0 +1,15 @@
+
+double Perimetr(double length, double width) {
+    double per = (length + width) * 2;
+    return per;
+}
+
+double Square(double length, double width) {
+    double sqr = length * width;
+    return sqr;
+}
+
+double Diagonal(double length, double width) {
+    double diag = sqrt(pow(length, 2) + pow(width, 2));
+    return diag;
+}
