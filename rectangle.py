4f89a08e276d67555456ffb8c3acf9f15e6c8747
Time:D <darya.sam54@gmail.com>
2024-04-03 00:17:51 +0700

    Cloned file from branch Vika

---
+++ rectangle.py
@@ -0,0 +1,5 @@
+def rect(array_sides):
+    perimetr = (array_sides[0] + array_sides[1])*2
+    square = array_sides[0] * array_sides[1]
+    diagonal = (pow(array_sides[0], 2)+ pow(array_sides[1], 2))**0.5
+    return perimetr, square, round(diagonal,5)
