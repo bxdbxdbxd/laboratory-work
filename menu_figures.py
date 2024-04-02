785de5e441c0a09451b4d672a62597aec26f86e7
Time:D <darya.sam54@gmail.com>
2024-04-02 20:40:13 +0700

    Match-case for choosing a figure

---
+++ menu_figures.py
@@ -0,0 +1,12 @@
+while(1):
+    match(input("Select a shape: \n1. Rectangle\n2. Triangle\n3. Trapezoid\n\n0 - Exit\n")):
+        case("1"):
+            pass
+        case("2"):
+            pass
+        case("3"):
+            pass
+        case("0"):
+            break
+        case _:
+            print("\nError\n")
