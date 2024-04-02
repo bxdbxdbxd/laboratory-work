f44bbe0401d629e7f800736c9944618871f76210
Time:D <darya.sam54@gmail.com>
2024-04-03 02:01:52 +0700

    Final ver fot float sides

--- rectangle.py
+++ rectangle.py
@@ -2,4 +2,4 @@ def rect(array_sides):
     perimetr = (array_sides[0] + array_sides[1])*2
     square = array_sides[0] * array_sides[1]
     diagonal = (pow(array_sides[0], 2)+ pow(array_sides[1], 2))**0.5
-    return perimetr, square, round(diagonal,5)
+    return round(perimetr, 5), round(square, 5), round(diagonal,5)
