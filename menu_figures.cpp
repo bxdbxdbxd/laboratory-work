033730c9052ea8f492b6193281131a8197f1a46b
Time:D <darya.sam54@gmail.com>
2024-04-03 13:16:55 +0700

    Modification

--- menu_figures.cpp
+++ menu_figures.cpp
@@ -9,31 +9,31 @@ int main() {
     int figure_option;
     double array_of_sides[4];
     do {
-    cout << "Select a shape: \n1. Rectangle\n2. Triangle\n3. Trapezoid" << endl;
+    cout << "Select a shape: \n1. Rectangle\n2. Triangle\n3. Trapezoid\n0 - Exit" << endl;
     cin >> figure_option;
     switch(figure_option) {
         case 1:
             filling_array(array_of_sides, 2);
-            cout << "Perimeter: " << Perimetr(array_of_sides[0], array_of_sides[1]) << "\nSquare: " << Square(array_of_sides[0], array_of_sides[1]) << "\nDiagonal: " << Diagonal(array_of_sides[0], array_of_sides[1]) << endl;
+            cout << "\nPerimeter: " << Perimetr(array_of_sides[0], array_of_sides[1]) << "\nSquare: " << Square(array_of_sides[0], array_of_sides[1]) << "\nDiagonal: " << Diagonal(array_of_sides[0], array_of_sides[1]) << endl << endl;
             break;
         case 2:
             double firstParties, secondParties, thirdParties;
             cin >> firstParties >> secondParties >> thirdParties;
             if (firstParties + secondParties > thirdParties && firstParties + thirdParties > secondParties && secondParties + thirdParties > firstParties) {
                 double sum = Perimetr(firstParties,secondParties,thirdParties);
-                cout << "Perimeter: " << sum << "\nSquare: " << squareGeron(firstParties,  secondParties, thirdParties, sum) << endl;
+                cout << "\nPerimeter: " << sum << "\nSquare: " << squareGeron(firstParties,  secondParties, thirdParties, sum) << endl;
                 if (checkIsosceles(firstParties,secondParties,thirdParties)) {
-                    cout<<"Triangle is isosceles"<<endl;
+                    cout<<"Triangle is isosceles"<<endl << endl;
                 } else{
-                    cout<<"Triangle is not isosceles"<<endl;
+                    cout<<"Triangle is not isosceles"<<endl << endl;
                 }
             } else{
-                cout<<"UNCORRECT";
+                cout<<"\nUNCORRECT\n";
             }
             break;
         case 3:
             filling_array(array_of_sides, 4);
-            cout << "Perimeter: " << trapezoid_perimeter(array_of_sides) << "\nSquare: " << area_of_trapezoid(array_of_sides) << "\nMidline lenth: " << midline_lenth(array_of_sides) << endl;
+            cout << "\nPerimeter: " << trapezoid_perimeter(array_of_sides) << "\nSquare: " << area_of_trapezoid(array_of_sides) << "\nMidline lenth: " << midline_lenth(array_of_sides) << endl << endl;
             break;
     }
     } while (figure_option != 0);
